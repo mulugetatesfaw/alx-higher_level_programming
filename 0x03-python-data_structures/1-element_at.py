e37843b2a54a4bf70cfecb28fed0b2b0bc72aa35
mulugetatesfaw <mulugetatesfaw21@gmail.com>
2023-01-06 09:56:47 -0800

    add element at an index

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    list_length = len(my_list) - 1
+    if (idx < 0 or idx >list_length):
+        return (None)
+    else:
+        return (my_list[idx])
+
+
+
+
