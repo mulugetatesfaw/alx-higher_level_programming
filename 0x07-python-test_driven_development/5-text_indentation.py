48bbcda48293dc7db1fc70f51c0962e4a911ce15
mulugetatesfaw <mulugetatesfaw21@gmail.com>
2023-01-31 22:10:21 -0800

    second fix

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,42 +1,17 @@
 #!/usr/bin/python3
-"""
-text identation module:
-indents text
-useful
-"""
+"""text indent"""
 
 
 def text_indentation(text):
-    """text must be a string, There should be no space
-    at the beginning
-    or at the end of each printed line.
-    """
-    # text is a string, otherwise raise a TypeError
-    if isinstance(text, str) is False:
+    """text indent"""
+    if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    # if text is None or not isinstance(text, str) or len(text) < 0:
-    # raise TypeError('must be a string')
-    # string.replace(old, new, count)
-    # prints a text with 2 new lines
-    text = text.replace('.', '.\n\n')
-    text = text.replace('?', '?\n\n')
-    text = text.replace(':', ':\n\n')
-    # 1) cut the entire line with \n
-    # 2) cut by separator with \n
-    # to the line that was strip (for.,?)
-    # loop text and split where "\n" is)
-    # end =""no space output promp line)
-    # there should be no spaces at the beginning
-    # and the end, line strip metodo
+    for delimeter in "?:.":
+        words = (delimeter + "\n\n").join(
+                [index.strip(" ") for index in words.split(delimeter)])
 
-    print("\n".join([line.strip() for line in text.split("\n")]), end="")
 
-    # if isinstance(text, str) is False:
-    # raise TypeError("text must be a string")
-    # delim = ":.?"
-    # for x in delim:
-    # text = text.replace(x, "{}\n\n".format(x))
-    # text = text.replace('\\', '')
-    # text = "\n".join([line.strip() for line in text.split('\n')])
-    # print(text, end="")
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/5-text_indentation.txt")
