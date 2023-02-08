750f3bdd9bb9f35767c47c0df37f4a0e75c051b7
mulugetatesfaw <mulugetatesfaw21@gmail.com>
2023-02-08 12:30:41 -0800

    fixed

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""Defines a file-writing function."""
 
 
 def write_file(filename="", text=""):
-    with open(filename, "w", encoding="UTF-8") as f:
+    """Write a string to a UTF"""
+    with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
