a7448d57c75d46a6e721543278871ec9fed975db
mulugetatesfaw <mulugetatesfaw21@gmail.com>
2023-03-14 15:26:18 -0700

    change to utf8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -3,4 +3,4 @@ ALTER DATABASE hbtn_0c_0 CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicod
 -- for table 
 ALTER TABLE first_table CONVERT TO CHARACHTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- for field names
-ALTER TABLE first_table CHANGE name name VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CHANGE name VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
