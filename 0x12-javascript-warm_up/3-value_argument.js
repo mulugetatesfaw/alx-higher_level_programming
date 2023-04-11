a9348a77ce1b5c381348c9d911bfdfd9cc94cc44
mulugetatesfaw <mulugetatesfaw21@gmail.com>
2023-04-11 01:34:22 -0700

    argument check

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-// prints the first argument passed to 
+// prints the first argument passed to ii depending the condition
 
 if (process.argv[2] === undefined) {
-	console.log('No argument');
+  console.log('No argument');
 } else {
-  console.log(process.argv[2};
+  console.log(process.argv[2]);
 }
