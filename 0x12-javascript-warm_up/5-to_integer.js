37c735bbab1b5c30a80d43d50609492dd2cdcc91
mulugetatesfaw <mulugetatesfaw21@gmail.com>
2023-04-11 01:43:04 -0700

    myNumber

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// prints the value passed depending the conditon
+
+if (isNaN(process.argv[2])) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + parseInt(process.argv[2]));
+}
