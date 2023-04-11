572110f91d299bd48f2b9459836d5f734b0b317f
mulugetatesfaw <mulugetatesfaw21@gmail.com>
2023-04-11 01:51:50 -0700

    looping_on

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,7 +2,7 @@
 // prints 3 lines: (like 1-multi_languages.js) but by
 // using an array of string and a loop
 
-const languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const lang = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
 for (let i = 0; i < 3; i++) {
   console.log(langs[i]);
